d75e970992491fcfe323b1227de7e6fc81a7ec81
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2022-03-28 19:35:16 +1100

    add a basic outline for this action

---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# determines the workflow conclusion for a workflow; aiming for this to be run
+# when a workflow is running.
+
+# funcs.
+die() { echo "$1" >&2; exit "${2:-1}"; }
+
+# check bash version.
+[[ "${BASH_VERSION:0:1}" -lt 4 ]] \
+  && die "bash version 4+ required"
+
+# check deps.
+deps=(curl jq)
+for dep in "${deps[@]}"; do
+  hash "$dep" 2>/dev/null || missing+=("$dep")
+done
+if [[ ${#missing[@]} -ne 0 ]]; then
+  s=""; [[ ${#missing[@]} -gt 1 ]] && { s="s"; }
+  die "missing dep${s}: ${missing[*]}"
+fi
+
+# check required parameters are given.
+token="$INPUT_TOKEN"
+missing=()
+[[ -z "$token" ]] && { missing+=("token"); }
+if [[ ${#missing[@]} -ne 0 ]]; then
+  [[ ${#missing[@]} -gt 1 ]] && { s="s"; }
+  die "missing input parameter${s}: ${missing[*]}"
+fi
+
+# vars.
+repo="${GITHUB_REPOSITORY}"
+[[ -z "$repo" ]] \
+  && die "missing \$GITHUB_REPOSITORY"
+id="${GITHUB_RUN_ID}"
+[[ -z "$id" ]] \
+  && die "missing \$GITHUB_RUN_ID"
+
+# retrieve conclusion for running workflow.
+echo "##[group]Retrieving conclusion for $repo $id"
+resp=$(curl -s "https://api.github.com/repos/$repo/actions/runs/$id" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: bearer $token") \
+  || die "failed curl to retrieve $repo workflow $id conclusion"
+conclusion=$(<<< jq -r '.conclusion') \
+  || die "failed to parse response from retrieving $repo workflow $id conclusion"
+echo "##[endgroup]"
+
+# print result.
+echo "##[group]Result"
+echo "$conclusion"
+echo "##[endgroup]"
+echo "::set-output name=conclusion::$conclusion"
